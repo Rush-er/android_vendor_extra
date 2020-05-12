0a387d2ea36e1cd5649c250fa03284239a326335
LuK1337 <priv.luk@gmail.com>
2020-05-13 00:01:38 +0200

    unconv: Install {Lato,Rubik} fonts and their overlay packages
    Adapted from LineageOS's change for new fonts_customization.xml implementation
    
    Change-Id: I3fe93982095722d22e726baf9561a6bde31f5bf1

--- product.mk
+++ product.mk
@@ -3,6 +3,8 @@ VENDOR_EXTRA_PATH := vendor/extra
 # Include Lawnchair 
 $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 $(call inherit-product-if-exists, external/jetbrainsmono/fonts.mk)
 
 # Custom packages
@@ -17,7 +19,10 @@ PRODUCT_PACKAGE_OVERLAYS += $(VENDOR_EXTRA_PATH)/overlay/common
 # Extra Font Overlays
 PRODUCT_PACKAGES += \
     FontJetBrainsMono \
+    FontLatoOverlay \
+    FontRubikOverlay
 
 # Add fonts styles to fonts-customization.xml
 ADDITIONAL_FONTS_FILE += \
-   external/jetbrainsmono/fonts-jetbrains.xml
+   external/jetbrainsmono/fonts-jetbrains.xml \
+   vendor/extra/fonts/fonts-unconv.xml
